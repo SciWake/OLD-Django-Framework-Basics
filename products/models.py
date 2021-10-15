0c4c0f0eff578de312dca49cf22cc5508b1aa803
Aleksey Kuritsyn <bimastics@gmail.com>
2021-10-15 14:53:27 +0300

    refactor: Overload __str__ method

--- products/models.py
+++ products/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
 
-# Create your models here.
-
 class ProductCategory(models.Model):
     name = models.CharField(max_length=64, unique=True)
     description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Product(models.Model):
@@ -16,3 +17,5 @@ class Product(models.Model):
     quantity = models.PositiveIntegerField(default=0)
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f'{self.name} | {self.category}'
