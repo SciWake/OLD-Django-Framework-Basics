0d4624a2dcf806a735ead611543c4a61fe2e9873
Aleksey Kuritsyn <bimastics@gmail.com>
2021-10-26 19:42:49 +0700

    fix: Protect access with @login_required decorator

--- users/views.py
+++ users/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import auth, messages
 from django.urls import reverse
-from django.contrib import auth
-from django.contrib import messages
 
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 from baskets.models import Basket
+
 
 def login(request):
     if request.method == 'POST':
@@ -35,6 +36,7 @@ def registration(request):
     return render(request, 'users/registration.html', context)
 
 
+@login_required
 def profile(request):
     if request.method == 'POST':
         form = UserProfileForm(instance=request.user, files=request.FILES, data=request.POST)
