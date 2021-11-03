8758c05b35796770f67c741817dc8d69630cb7ff
Aleksey Kuritsyn <bimastics@gmail.com>
2021-11-04 00:39:54 +0700

    feat: Implement updating users in the admin panel

--- my_admin/views.py
+++ my_admin/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
 
 from users.models import User
-from my_admin.forms import UserAdminRegistrationForm
+from my_admin.forms import UserAdminRegistrationForm, UserAdminProfileForm
 
 
 def index(request):
@@ -27,8 +27,16 @@ def admin_users(request):
     return render(request, 'my_admin/admin-users-read.html', context)
 
 
-def admin_users_update(request):
-    context = {'title': 'GeekShop - Обновление пользователя'}
+def admin_users_update(request, id):
+    selected_user = User.objects.get(id=id)
+    if request.method == 'POST':
+        form = UserAdminProfileForm(instance=selected_user, files=request.FILES, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('my_admin:admin_users'))
+    else:
+        form = UserAdminProfileForm(instance=selected_user)
+    context = {'title': 'Админ-панель - Обновление пользовтаеля', 'form': form, 'selected_user': selected_user}
     return render(request, 'my_admin/admin-users-update-delete.html', context)
 
 
