0cedbe3e304bd854b81d7ec139ac8d040fa80599
Aleksey Kuritsyn <bimastics@gmail.com>
2021-11-03 23:12:05 +0700

    feat: Implement displaying users in the admin panel

--- my_admin/views.py
+++ my_admin/views.py
@@ -1,24 +1,26 @@
 from django.shortcuts import render
 
+from users.models import User
+
 
 def index(request):
     context = {'title': 'GeekShop - Панель Администратора'}
-    return render(request, 'my_admin/index.html',  context)
+    return render(request, 'my_admin/index.html', context)
 
 
 def admin_users_create(request):
     context = {'title': 'GeekShop - Создание пользователей'}
-    return render(request, 'my_admin/admin-users-create.html',  context)
+    return render(request, 'my_admin/admin-users-create.html', context)
 
 
 def admin_users(request):
-    context = {'title': 'GeekShop - Пользователи'}
-    return render(request, 'my_admin/admin-users-read.html',  context)
+    context = {'title': 'GeekShop - Пользователи', 'users': User.objects.all()}
+    return render(request, 'my_admin/admin-users-read.html', context)
 
 
 def admin_users_update(request):
     context = {'title': 'GeekShop - Обновление пользователя'}
-    return render(request, 'my_admin/admin-users-update-delete.html',  context)
+    return render(request, 'my_admin/admin-users-update-delete.html', context)
 
 
 def admin_users_delete(request):
