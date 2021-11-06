67059f91601c8499b2d98a9e4c285df6acad16a5
Aleksey Kuritsyn <bimastics@gmail.com>
2021-11-06 18:06:12 +0700

    reafctor: Create UserCreateView

--- my_admin/urls.py
+++ my_admin/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 
-from my_admin.views import index, UserListView, admin_users_update, admin_users_delete, admin_users_create
+from my_admin.views import index, UserListView, admin_users_update, admin_users_delete, UserCreateView
 
 app_name = 'my_admin'
 
 urlpatterns = [
     path('', index, name='index'),
     path('users/', UserListView.as_view(), name='admin_users'),
-    path('users-create/', admin_users_create, name='admin_users_create'),
+    path('users-create/', UserCreateView.as_view(), name='admin_users_create'),
     path('users-update/<int:id>/', admin_users_update, name='admin_users_update'),
     path('users-delete/<int:id>/', admin_users_delete, name='admin_users_delete'),
 ]
--- my_admin/views.py
+++ my_admin/views.py
@@ -1,21 +1,11 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.contrib.auth.decorators import user_passes_test
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
 
 from users.models import User
 from my_admin.forms import UserAdminRegistrationForm, UserAdminProfileForm
-
-
-class UserListView(ListView):
-    model = User
-    template_name = 'my_admin/admin-users-read.html'
-
-
-# @user_passes_test(lambda user: user.is_staff)
-# def admin_users(request):
-#     context = {'title': 'GeekShop - Пользователи', 'users': User.objects.all()}
-#     return render(request, 'my_admin/admin-users-read.html', context)
 
 
 @user_passes_test(lambda user: user.is_staff)
@@ -24,17 +14,16 @@ def index(request):
     return render(request, 'my_admin/index.html', context)
 
 
-@user_passes_test(lambda user: user.is_staff)
-def admin_users_create(request):
-    if request.method == 'POST':
-        form = UserAdminRegistrationForm(data=request.POST, files=request.FILES)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse('my_admin:admin_users'))
-    else:
-        form = UserAdminRegistrationForm()
-    context = {'title': 'Админ-панель - Создание пользователя', 'form': form}
-    return render(request, 'my_admin/admin-users-create.html', context)
+class UserListView(ListView):
+    model = User
+    template_name = 'my_admin/admin-users-read.html'
+
+
+class UserCreateView(CreateView):
+    model = User
+    form_class = UserAdminRegistrationForm
+    success_url = reverse_lazy('my_admin:admin_users')
+    template_name = 'my_admin/admin-users-create.html'
 
 
 @user_passes_test(lambda user: user.is_staff)
@@ -56,3 +45,21 @@ def admin_users_delete(request, id):
     user = User.objects.get(id=id)
     user.safe_delete()
     return HttpResponseRedirect(reverse('my_admin:admin_users'))
+
+# @user_passes_test(lambda user: user.is_staff)
+# def admin_users(request):
+#     context = {'title': 'GeekShop - Пользователи', 'users': User.objects.all()}
+#     return render(request, 'my_admin/admin-users-read.html', context)
+
+
+# @user_passes_test(lambda user: user.is_staff)
+# def admin_users_create(request):
+#     if request.method == 'POST':
+#         form = UserAdminRegistrationForm(data=request.POST, files=request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponseRedirect(reverse('my_admin:admin_users'))
+#     else:
+#         form = UserAdminRegistrationForm()
+#     context = {'title': 'Админ-панель - Создание пользователя', 'form': form}
+#     return render(request, 'my_admin/admin-users-create.html', context)
